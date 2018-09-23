2d34dccad5a4404634d243fa0d6161b303e7bce1
dayo92 <dan.ataiyero@hotmail.com>
2018-09-23 18:34:01 +0100

    updated dependencies + query url, reverse param

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const west = { name: 'Daniel', age:26, cool:true}
+  // res.send('Hey! It works!');
+  // res.json(west);
+  // res.send(req.query.name);
+  res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  // res.send('it works!')
+  // res.send(req.params.name)
+  res.send(reverse);
 });
 
 module.exports = router;
